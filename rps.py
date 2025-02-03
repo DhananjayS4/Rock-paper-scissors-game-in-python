b5592a009fb895021905484f31fb9ce06da93daa
Goutham <167563063+Goutham-BS@users.noreply.github.com>
2025-02-04 00:11:01 +0530

    Added computer-generated dynamic responses and few other text changes.

--- rps.py
+++ rps.py
@@ -4,32 +4,47 @@ user_count= 0
 computer_count=0
 options=["rock","paper","scissors"]
 
+cwin = ["Imaging losing XD", "What can I say, I'm just too good at this ;D", "You should just quit XD", "You'll catch up don't worry :3"]
+uwin = ["I'll get there soon enough", "You just got lucky...", "Are you cheating???", "I'm reporting you >:(", "Are you a hacker?"]
+draw = ["Jinx!", "Like minds think alike ;)","Should've gone with my gut feeling...", "déjà vu"]
+
+def score():
+        print("\n")
+        print("Current score:")
+        print("You\tComputer")
+        print(f"{user_count}\t{computer_count}")
+        print("\n")
+
+print("Computer: Hello there! Let's play ^-^\n")
 while(True):
-    user_input=input("Type Rock/ Paper or Scissors. Q to quit the game.\n").lower()
+    user_input=input("Type Rock, Paper or Scissors. Type Q to exit the game.\n>> ").lower()
+
     if user_input=='q':
         print("Thank you for playing! This window will automatically close in 5 seconds.")
+        print("This was fun! See you soon! ;)")
         time.sleep(5)
         break
+
+    elif user_input=='score':
+        score()
+        continue
+
     elif user_input not in options:
-        print("Plese enter the valid option: ")
+        print("Invalid option. Try again.\n ")
         continue 
 
-    random_number=random.randint(0,2)
-    computer_choice=options[random_number]
-    print("Computer choice: ",computer_choice,".")
+    computer_choice=random.choice(options)
+    print(f"\nComputer's choice: {computer_choice}")
 
     if user_input==computer_choice:
-        print("It's a tie!")
-        user_count+=1
-        computer_count+=1
+        print("It's a draw!")
+        print(f"\nComputer: {random.choice(draw)}\n")
 
     elif (user_input=='rock' and computer_choice=='scissors') or (user_input=='scissors' and computer_choice=='paper') or (user_input=='paper' and computer_choice=='rock'):
         print("You win!")
         user_count+=1
+        print(f"\nComputer: {random.choice(uwin)}\n")
     else:
-        print("Computer Won!")
+        print("Computer Wins!")
         computer_count+=1
-
-    print("You won ",user_count," times.")
-    print("Computer won ",computer_count," times.")
-    print("Well played!")
+        print(f"\nComputer: {random.choice(cwin)}\n")
